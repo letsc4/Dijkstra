73720abc24d505840c41e8b513dfabe2a8069d57
letsc4 <77798517+letsc4@users.noreply.github.com>
2022-12-05 22:59:52 +0100

    Update points.py

--- points.py
+++ points.py
@@ -1,18 +1,7 @@
-class Point():
-    def __init__(self, id, relations):
-        self.id = id
-        self.relations = relations
+from dataclasses import dataclass
 
-    @property
-    def relations(self):
-        return self._relations
 
-    @relations.setter
-    def relations(self, value):
-        self._relations = value
-
-    def __str__(self) -> str:
-        return self.id
-
-    def __eq__(self, __o: object) -> bool:
-        return self.id == __o
+@dataclass()
+class Point:
+    id: str
+    relations: dict
