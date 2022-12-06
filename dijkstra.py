dd7030284d58532c93e8fcdb7fc8b7996ba5a370
letsc4 <77798517+letsc4@users.noreply.github.com>
2022-12-06 07:06:33 +0000

    need to change a lot

--- dijkstra.py
+++ dijkstra.py
@@ -1,11 +1,16 @@
-class LowestDistanceNode():
+class Graph():
+
     def __init__(self, start_node, nodes):
         self.__start_node = start_node
         self.__nodes = nodes
-        self._visited_nodes = []
-        self.visited_nodes = start_node
+
         self._node_distances = {}
-        self.node_distances = nodes
+        for i in nodes:
+            self._node_distances.update({i.id: {}})
+        # for j in start_node.relations:
+        #     self._node_distances[j].update({j: start_node.relations[j]})
+
+        self._visited_nodes = []
 
     @property
     def visited_nodes(self):
@@ -20,33 +25,9 @@ class LowestDistanceNode():
         return self._node_distances
 
     @node_distances.setter
-    def node_distances(self, nodes):
-        if isinstance(nodes, tuple):  # if node is tuple of nodes; for initalization
-            for i in nodes:
-                self._node_distances.update(
-                    {i.id: None})  # CHANGE NONE TO WORK
-            for o in self.__start_node.relations:
-                self._node_distances.update(
-                    {o: self.__start_node.relations[o]})
-        else:  # if node is point object
-            add_onto = self.__add_onto(nodes)
-
-            # update values of visited notes with logic(dont update key when value higher)
-            for neighbour in nodes.relations:
-
-                if neighbour == self.__start_node.id:
-                    continue
-                elif self._node_distances[neighbour] == None:
-                    self._node_distances.update(
-                        {neighbour: nodes.relations[neighbour] + add_onto})
-                elif nodes.relations[neighbour] + add_onto < self._node_distances[neighbour]:
-                    self._node_distances.update(
-                        {neighbour: nodes.relations[neighbour] + add_onto})
-
-            self._visited_nodes.append(nodes)
-
-    def __add_onto(self, node):
-        return self._node_distances[node.id]
+    def node_distances(self, current_node, neighbour):
+        self._node_distances[neighbour].update(
+            {neighbour: current_node.relations[neighbour]})
 
     @property
     def lowest_distance(self):
@@ -61,9 +42,10 @@ class LowestDistanceNode():
 
         return lowest_key  # must be string
 
-    @property
-    def count_of_objects(self):
-        return len(self.__nodes) + 1
+    # @property
+    # def count_of_objects(self):
+    #     return len(self.__nodes) + 1
+
 
 
 def dijkstra(start, *other):
