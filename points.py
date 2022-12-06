95848309de321fec4e94a07f3a5435f2210ae60e
letsc4 <77798517+letsc4@users.noreply.github.com>
2022-12-06 08:16:42 +0100

    revert

--- points.py
+++ points.py
@@ -1,7 +1,18 @@
-from dataclasses import dataclass
+class Point():
+    def __init__(self, id, relations):
+        self.id = id
+        self.relations = relations
 
+    @property
+    def relations(self):
+        return self._relations
 
-@dataclass()
-class Point:
-    id: str
-    relations: dict
+    @relations.setter
+    def relations(self, value):
+        self._relations = value
+
+    def __str__(self) -> str:
+        return self.id
+
+    def __eq__(self, __o: object) -> bool:
+        return self.id == __o
